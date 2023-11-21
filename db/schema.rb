384d7ee4cdf43620cca520cf71f7e312d3595ef0
Cwabla <cwabla.amewuga@gmail.com>
2023-11-21 02:16:00 -0500

    new1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_22_191607) do
+ActiveRecord::Schema.define(version: 2023_11_20_232234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -152,31 +152,12 @@ ActiveRecord::Schema.define(version: 2023_07_22_191607) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-    t.integer "admin"
-    t.string "basic_auth_username", null: false
-    t.string "basic_auth_password", null: false
-    t.string "image_password", null: false
-    t.index ["basic_auth_username"], name: "index_users_on_basic_auth_username", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "feed_entries", "feeds"
-  add_foreign_key "feeds", "users"
-  add_foreign_key "indie_auth_requests", "users"
   add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users"
-  add_foreign_key "login_links", "users"
-  add_foreign_key "posts", "users", column: "author_id"
 end
